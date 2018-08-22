dbcc251da75e5d911e6ab1ccb7ea42cbe901efe0
Zachary Stone <zacharythomasstone89@gmail.com>
2018-08-22 16:07:05 -0400

    Cleaned up code and added more dialog
    also added the ability to store the users last guess and display it to the console.
    
    Cleaned up code, added comments. Also cleared console between guesses to keep the console clean and easy to read.

--- GuessingGame/GuessingGame1.cs
+++ GuessingGame/GuessingGame1.cs
@@ -10,43 +10,57 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            //stores the number randomly picked that the player must guess.
             int theAnswer;
+            //stores players guesses.
             int playerGuess;
+            //used to store the last guess the user guessed. 
+            int lastGuess = 0;
+            //stores the amount of times the user attempts a guess.
             int numberOfAttemps = 0;
+            //stores the difficulty level.
             string difficulty;
+            //stores the input from user.
             string playerInput;
+            //stores the users name.
             string playerName;
+            //checks to see if the user guessed the winning number
             bool isNumberGuessed = false;
+            //checks to ensure the user has set the difficulty level.
             bool isDifficultySet = false;
+
+            //Start of game
             Console.WriteLine("The Guessing Game! Enter q at anytime to quit!");
             Console.WriteLine("What is your name?");
             playerName = Console.ReadLine();
-          
-              
+            Console.Clear();
+
+
             do
             {
                 //Asking User to Set Difficulty 
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("{0}, Choose a difficulty: easy, normal, or hard?", playerName);
                 difficulty = Console.ReadLine();
+                Console.Clear();
                 //Checking if User Input is to quit.
                 if (difficulty == "q")
                     break;
 
-
                 if (difficulty == "easy")
                 {
-
+                    //generates the answer for easy difficulty setting.
                     Random r = new Random();
                     theAnswer = r.Next(1, 6);
                     isDifficultySet = true;
 
                     do
                     {
-                        // get player guess
+                        //get player guess.
                         Console.ForegroundColor = ConsoleColor.White;
                         Console.WriteLine("{0}, enter your guess (1-5): ", playerName);
                         playerInput = Console.ReadLine();
+                        Console.Clear();
                         numberOfAttemps++;
                         if (playerInput == "q")
                             break;
@@ -69,21 +83,33 @@ namespace GuessingGame
                             else
                             {
                                 Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Wrong Answer, {0}!", playerName);
+
                                 if (playerGuess > theAnswer && playerGuess >= 1 && playerGuess <= 5)
                                 {
+                                    lastGuess = playerGuess;
                                     Console.WriteLine("Too High!");
 
                                 }
                                 else if (playerGuess < theAnswer && playerGuess >= 1 && playerGuess <= 5)
                                 {
+                                    lastGuess = playerGuess;
                                     Console.WriteLine("Too Low!");
 
                                 }
                                 else if (playerGuess > 5 || playerGuess < 1)
                                 {
+                                    lastGuess = playerGuess;
                                     Console.WriteLine("Number is out of bounds!");
                                 }
+                                if (lastGuess != 0)
+                                {
+                                    Console.WriteLine("Wrong Answer, {0}! Last Guess: {1}.", playerName, lastGuess);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Wrong Answer, {0}!", playerName);
+                                }
+                                Console.WriteLine("Guess again.");
                             }
                         }
                         else
@@ -144,6 +170,7 @@ namespace GuessingGame
                                 {
                                     Console.WriteLine("Number is out of bounds!");
                                 }
+                                Console.WriteLine("Guess again.");
                             }
                         }
                         else
@@ -204,6 +231,7 @@ namespace GuessingGame
                                 {
                                     Console.WriteLine("Number is out of bounds!");
                                 }
+                                Console.WriteLine("Guess again.");
                             }
                         }
                         else
@@ -220,7 +248,7 @@ namespace GuessingGame
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("That is not a valid difficulty, {0}!", playerName);
                 }
-              
+
             } while (!isDifficultySet);
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Thank you for playing!");
